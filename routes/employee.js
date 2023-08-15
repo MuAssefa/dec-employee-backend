a8a8501e91b08d5ec3f1e465965425a9f04c54e7
Muluken <muluken@thepracticalit.com>
2023-08-15 01:39:19 -0400

    added get all employees endpoint

--- routes/employee.js
+++ routes/employee.js
@@ -57,4 +57,13 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+router.get("/employees", async (req, res) => {
+  try {
+    const getAllEmployees = await Employee.find({});
+    res.status(200).json(getAllEmployees);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
